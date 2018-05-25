4fa350aeedae08a6ebb8c2b745b269aa9d0ec054
Alex Jurkiewicz <alexj@kounta.com>
2018-05-25 15:46:46 +1000

    Fix player display (still ugly)

--- website/src/Ranking.js
+++ website/src/Ranking.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 
+function precisionRound(number, precision) {
+  var factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
+}
+
+function playerRankEntry(player) {
+  return <li>{player.PlayerName}: {precisionRound(player.mu, 2)} mmr</li>;
+}
+
 class Ranking extends Component {
   constructor(props) {
     super(props);
-    this.state = {rankingData: {}};
+    this.state = {};
     fetch('https://api.foosweb.jurkiewi.cz/player')
       .then(function(response) {
-        console.log(response);
-        this.state.rankingData = response.body;
+        return response.json();
       })
+      .then(function(data) {
+        console.log(data);
+        data = data.sort((a, b) => {
+          return b.mu - a.mu;
+        });
+        this.setState({
+          "players": data
+        });
+      }.bind(this));
   }
+
   render() {
+    let players = []
+    if (this.state.players !== undefined) {
+      players = this.state.players;
+    }
+    const newPlayers = players.map(playerRankEntry);
+    console.log(players);
+    console.log(newPlayers);
     return (
       <div className="Ranking">
-        Here is the player ranking:
+        <p>Current player ranking:</p>
+        <ol>
+          {newPlayers}
+        </ol>
+
       </div>
     );
   }
--- website/src/kounta-data.js
+++ website/src/kounta-data.js
@@ -4,5 +4,6 @@ export const KountaPlayers = [
   'Jamie',
   'Brendan',
   'Jingbo',
-  'Greg'
+  'Greg',
+  'Nick'
 ];
